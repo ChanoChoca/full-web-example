28472832095c4c1127776d2f8b241f81bdf0b759
Juan Ignacio Caprioli <juanicaprioli16@outlook.com>
2025-11-22 11:10:24 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="overflow-hidden">
         <LenisProvider>
           <Navbar />
           {children}
